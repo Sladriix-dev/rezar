257e40f797db1f3de20a8d8e47f2c9b197edb2eb
Sladriix-dev <sladriix@gmail.com>
2022-05-05 10:55:11 +0200

    [add] product & homescreen component /w css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,32 @@
 import React from "react";
-import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Rezar
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Panier</a>
-          <a href="/signin">S'inscrire</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img 
-                  className="medium" 
-                  src={product.image} 
-                  alt={product.name} />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Rezar
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Panier</a>
+            <a href="/signin">S'inscrire</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} exact />
+          </Routes>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
