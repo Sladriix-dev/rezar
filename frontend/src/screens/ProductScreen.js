8de39de98d614d313a61ef136732401686d15a97
Sladriix-dev <sladriix@gmail.com>
2022-05-06 10:56:01 +0200

    [add] redux in ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,67 +1,83 @@
-import React from 'react'
-import Rating from '../components/Rating';
-import data from '../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import Rating from "../components/Rating";
+import { Link, useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const { id: productId } = params;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-    const { id } = useParams();
-    const product = data.products.find((x) => x._id === id);
-    if(!product) {
-        return <div>Product Not Found</div>;
-    }
-    return (
-      <div>
-        <Link to="/">Revenir à la boutique</Link>
-        <div className="row top">
-          <div className="col-2">
-            <img className="large" src={product.image} alt={product.name} />
-          </div>
-          <div className="col-1">
-            <ul className='desc_product'>
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Revenir à la boutique</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img className="large" src={product.image} alt={product.name} />
+            </div>
+            <div className="col-1">
+              <ul className="desc_product">
                 <li>
-                    <h1>{product.name}</h1>
+                  <h1>{product.name}</h1>
                 </li>
                 <li>
-                    <Rating 
-                        rating={product.rating}
-                        numReviews={product.numReviews}
-                    />
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  />
                 </li>
                 <li>
-                    <p>{product.description}</p>
+                  <p>{product.description}</p>
                 </li>
-            </ul>
-          </div>
-          <div className="col-1">
-            <div className='card card-body'>
+              </ul>
+            </div>
+            <div className="col-1">
+              <div className="card card-body">
                 <ul>
-                    <li>
-                        <div className='row'>
-                            <div>Prix</div>
-                            <div className='price'>${product.price}</div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='row'>
-                            <div>Status</div>
-                            <div>
-                                {product.countInStock > 0 ? ( <span className="success">En Stock</span>
-                                ) : (  
-                                    <span className='danger'>En Rupture</span>
-                                )}
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <button className="primary block">Ajouter au panier</button>
-                    </li>
+                  <li>
+                    <div className="row">
+                      <div>Prix</div>
+                      <div className="price">${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">En Stock</span>
+                        ) : (
+                          <span className="danger">En Rupture</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Ajouter au panier</button>
+                  </li>
                 </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      )}
+    </div>
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
